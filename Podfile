25aa9de9bbfe13fdaf1f2338379d3ad214e71b4c
BumMo Koo <ksquareatm@gmail.com>
2015-07-01 13:28:04 +0900

    [Pod] Integrated Google Cloud Messaging.

---
+++ Podfile
@@ -0,0 +1,6 @@
+workspace 'GCM-Test'
+xcodeproj 'GCM-Test/GCM-Test.xcodeproj'
+
+target 'GCM-Test' do
+	pod 'Google/CloudMessaging'
+end
---
+++ Pods/Target Support Files/Pods-GCM-Test/Pods-GCM-Test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GCM_Test : NSObject
+@end
+@implementation PodsDummy_Pods_GCM_Test
+@end
