a4bca54bdc569bac168c3d38de7b09301f0d325c
BumMo Koo <ksquareatm@gmail.com>
2015-07-01 13:21:01 +0900

    Added workspace and project.

---
+++ GCM-Test/GCM-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GCM-Test
+//
+//  Created by 구범모 on 2015. 7. 1..
+//  Copyright (c) 2015년 gbmKSquare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
